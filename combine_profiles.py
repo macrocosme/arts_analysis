efdc627673d77ac84b7126fa19bc7a61f83512d5
Liam Connor <liam.dean.connor@gmail.com>
2016-11-28 12:26:04 +0100

    Pythonic version of Leon's combine_profile shell script

---
+++ combine_profiles.py
@@ -0,0 +1,56 @@
+import os
+
+import numpy as np
+import glob
+import argparse
+
+import psrchive
+
+parser = argparse.ArgumentParser()
+parser.add_argument("date", help="date of observation in yyyymmdd format")
+parser.add_argument("folder", help="subfolder of data/*/Timing/yyyymmdd/\
+									that contains folded profiles")											
+parser.add_argument("-sband", help="start band number", default="1", type=int)	
+parser.add_argument("-eband", help="end band number", default="16", type=int)
+parser.add_argument("-o", help="name of output file name", default="all.ar")
+args = parser.parse_args()
+
+date, folder, sband, eband, outnamee = args.date, args.folder, args.sband, args.eband, args.o 
+# Demand that band number is two digits
+# sband = "%02d"%args.sband
+# eband = "%02d"%args.eband
+
+def combine_in_time(sband=1, eband=16):
+
+	for band in range(sband, eband+1):
+                band = "%02d"%band
+		fullpath = "/data/%s/Timing/%s/%s" % (band, date, folder)
+		print "Processing %s" % band
+
+		file_list = glob.glob(fullpath + "/*.ar")
+
+		data_time_average = []
+
+		for fn in file_list:
+			arch = psrchive.Archive_load(fn)
+			data = arch.get_data()
+			data_time_average.append(data)
+
+		data_time_average = np.concatenate(data_time_average, axis=0).sum(axis=0)
+
+def combine_in_time_shell(sband=1, eband=16):
+
+	for band in range(sband, eband+1):
+                band = "%02d"%band
+		fullpath = "/data/%s/Timing/%s/%s" % (band, date, folder)
+
+		os.system("(nice psradd -P %s/*.ar -o band%s.ar; psredit -m -c bw=18.75 band%s.ar) &" 
+		 			% (fullpath, band, band))
+
+                print "Done %s" % band
+
+combine_in_time_shell(sband, eband)
+
+
+
+#fullpath = "/data/%s/Timing/%s/%s" % (band, date, folder)
