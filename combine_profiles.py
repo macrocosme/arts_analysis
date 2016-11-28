3a54a666253b793bffccebf877ac837c178c3c27
Liam Connor <connor@MacConnor.local>
2016-11-28 18:46:09 +0100

    testing subsets of directories

--- combine_profiles.py
+++ combine_profiles.py
@@ -6,17 +6,29 @@ import argparse
 
 import psrchive
 
-parser = argparse.ArgumentParser()
-parser.add_argument("date", help="date of observation in yyyymmdd format")
-parser.add_argument("folder", help="subfolder of data/*/Timing/yyyymmdd/\
-									that contains folded profiles")											
-parser.add_argument("-sband", help="start band number", default="1", type=int)	
-parser.add_argument("-eband", help="end band number", default="16", type=int)
-parser.add_argument("-o", help="name of output file name", default="all.ar")
-args = parser.parse_args()
 
-date, folder = args.date, args.folder
-sband, eband, outnamee = args.sband, args.eband, args.o 
+def combine_in_time_(filepath, band, date, subint='', outfile='band'):
+
+	os.system("(nice psradd -P %s -o %s.ar; psredit -m -c bw=18.75 %s.ar) &" 
+		 		% (filepath, outfile + band, outfile + band))
+
+def combine_allbands(sband=1, eband=16):
+
+	for band in range(sband, eband+1):
+		band = "%02d"%band
+
+		for xx in range(5):
+			xx = str(xx)
+			fullpath = "/data/%s/Timing/%s/%s" % (band, date, folder)
+			filepath = '%s/*%s*.ar' % (fullpath, '_1'+xx)
+			print filepath
+			combine_in_time_(filepath, band, date, subint='_'+xx, outfile=xx + 'band')
+
+		subintfiles = './*band%s.ar' % band
+		outfile = 'time_averaged_%s_%s' % (date, band)
+		combine_in_time_(subintfiles, band, date, outfile=outfile)
+
+        print "Done %s" % band
 
 def combine_in_time(sband=1, eband=16):
 
@@ -36,30 +48,20 @@ def combine_in_time(sband=1, eband=16):
 
 		data_time_average = np.concatenate(data_time_average, axis=0).sum(axis=0)
 
-def combine_in_time_(filepath, band, date, subint='', outfile='band'):
 
-	os.system("(nice psradd -P %s -o %s.ar; psredit -m -c bw=18.75 %s.ar &)" 
-		 		% (filepath, outfile + band, outfile + band))
+if __name__=='__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument("date", help="date of observation in yyyymmdd format")
+	parser.add_argument("folder", help="subfolder of data/*/Timing/yyyymmdd/\
+										that contains folded profiles")											
+	parser.add_argument("-sband", help="start band number", default="1", type=int)	
+	parser.add_argument("-eband", help="end band number", default="16", type=int)
+	parser.add_argument("-o", help="name of output file name", default="all.ar")
+	args = parser.parse_args()
 
-def combine_allbands(sband=1, eband=16):
-
-	for band in range(sband, eband+1):
-		band = "%02d"%band
-
-		for xx in range(10):
-			xx = str(xx)
-			fullpath = "/data/%s/Timing/%s/%s" % (band, date, folder)
-			filepath = '%s/*%s*.ar' % (fullpath, '_1'+xx)
-			print filepath
-			combine_in_time_(filepath, band, date, subint='_'+xx, outfile=xx + 'band')
-
-		subintfiles = './*band%s.ar' % band
-		outfile = 'time_averaged_%s_%s' % (date, band)
-		combine_in_time_(subintfiles, band, date, outfile=outfile)
-
-        print "Done %s" % band
-
-combine_allbands(sband, eband)
+	date, folder = args.date, args.folder
+	sband, eband, outnamee = args.sband, args.eband, args.o 
+	combine_allbands(sband, eband)
 #combine_in_time(sband, eband)
 
 #fullpath = "/data/%s/Timing/%s/%s" % (band, date, folder)
