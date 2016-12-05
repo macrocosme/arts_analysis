8e0abe790bc8d770a823336d105842db9959f75e
Liam Connor <connor@wcw-staff-145-18-168-244.wireless.uva.nl>
2016-12-05 12:01:53 +0100

    testing subsets of directories

--- combine_profiles.py
+++ combine_profiles.py
@@ -165,11 +165,11 @@ if __name__=='__main__':
 
 	combine_subints(sband, eband, subints=subints, outfile='time_averaged'+folder)
 
-	for band in range(sband, eband+1):
-		print "collecting %s" % band
+	# for band in range(sband, eband+1):
+	# 	print "collecting %s" % band
 
-		band = "%02d"%band
-		dedisperse_folded_spec('time_averaged'+folder+subints+'band'+band)
+	# 	band = "%02d"%band
+	# 	dedisperse_folded_spec('time_averaged'+folder+subints+'band'+band)
 
 	combine_freq(fnames='time_averaged'+folder, outfile=outname+folder+'.ar')
 #	dedisperse_folded_spec(outname+folder)
