bbc4cd40ce4b16f42a23d494600fb593148e3366
Liam Connor <connor@macconnor.nfra.nl>
2016-12-07 12:03:25 +0100

    Added manual dedispersion

--- combine_profiles.py
+++ combine_profiles.py
@@ -253,10 +253,11 @@ if __name__=='__main__':
 
 		ax4 = fig.add_subplot(414)
 		ph = np.linspace(0, p0, len(prof_2dm))
-		plt.plot(ph, prof_0dm)
-		plt.plot(ph, prof_dm)
-		plt.plot(ph, prof_2dm)
-
+		plt.plot(ph, prof_0dm, color='steelblue')
+		plt.plot(ph, prof_dm, color='lightslategrey')
+		plt.plot(ph, prof_2dm, color='cornflowerblue')
+		plt.legend(["DM=0", "DM=expected", "DM=2*expected"])
+		plt.xlim(0, p0)
 		plt.xlabel('pulse phase [s]')
 
 		plt.show()
