fab6f63f792d520e7882431d57f08b4ab4c4c324
Liam Connor <connor@wcw-staff-145-109-90-199.wireless.uva.nl>
2018-02-02 16:23:21 +0100

    tmin tmax bug

--- triggers.py
+++ triggers.py
@@ -245,7 +245,7 @@ def proc_trigger(fn_fil, dm0, t0, sig_cut,
 
     if chunksize > ntime_plot*downsamp:
 #        t_min, t_max = chunksize//2-5000, chunksize//2+5000
-        t_min, t_max = chunksize//2-ntime_plot//2, chunksize+ntime_plot//2
+        t_min, t_max = chunksize//2-ntime_plot//2, chunksize//2+ntime_plot//2
     else:
         t_min, t_max = 0, chunksize
 
