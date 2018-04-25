32464fbcd134e3da00db587dd66263d47a2c69a9
ARTS <arts@ARTS0>
2018-04-25 18:30:54 +0000

    changing injection script to loop

--- injection_script.py
+++ injection_script.py
@@ -1,26 +1,43 @@
 import os
+import time
 
 import glob
 
-#outdir = '/data/03/Triggers/'
-#infile = '/data/03/Triggers/CB_random.fil'
-infile = '/data2/output/20180402/2018-04-02-09:40:11.M31/filterbank/CB22.fil'
-infile = '/data2/output/20180328/2018-03-28-13:01:20.J0248+6021/filterbank/CB21.fil'
+N_FRB = 5
+SNR_MIN = 8
+backend = 'PRESTO'
+AMBER_PATH = '~/test/amber_arg.sh'
+
+outdir = '/data/03/Triggers/'
+infile = '/data/03/Triggers/CB_random.fil'
+#infile = '/data2/output/20180402/2018-04-02-09:40:11.M31/filterbank/CB22.fil'
+#infile = '/data2/output/20180328/2018-03-28-13:01:20.J0248+6021/filterbank/CB21.fil'
 #infile = '/data1/output/20180425/2018-04-25-03\:02\:05.RA20DEC57/filterbank/CB21.fil'
-infile = '/data2/output/snr_tests_liam/CB21.fil'
-outdir = '/data2/snr_tests_liam'
-NFRB = 100
+#infile = '/data2/output/snr_tests_liam/CB21.fil'
+#outdir = '/data2/snr_tests_liam'
 
-fn250 = 'dm250_%sfrbs' % NFRB
-fn500 = 'dm500_%sfrbs' % NFRB
-fn1000 = 'dm1000_%sfrbs' % NFRB
-fn2500 = 'dm2500_%sfrbs' % NFRB
+timestr = time.strftime("%Y%m%d-%H%M")
+#os.system('python inject_frb.py %s %s --nfrb %d \
+#          --dm_list 100.0,250.0,500.0,750.0,1000.0,1500.0' \
+#          % (infile, outdir, N_FRB))
 
-os.system('python inject_frb.py %s %s/%s --nfrb %d --dm_high 250.0 --calc_snr True' % (infile, outdir, fn250, NFRB))
-os.system('python inject_frb.py %s %s/%s --nfrb %d --dm_high 500.0' % (infile, outdir, fn500, NFRB))
-os.system('python inject_frb.py %s %s/%s --nfrb %d --dm_high 1000.0 --calc_snr True' % (infile, outdir, fn1000, NFRB))
-os.system('python inject_frb.py %s %s/%s --nfrb %d --dm_high 2500.0' % (infile, outdir, fn2500, NFRB))
+timestr = '20180425-1742'
+# note this assumes tstr is the same in both inject_frb and glob
+fil_list = glob.glob('%s/*%s.fil' % (outdir, timestr))
 
+for fn_fil in fil_list:
+    DM = float(fn_fil.split('dm')[1].split('_')[0])
+    fn_base = fn_fil.strip('.fil')
+
+    if backend is 'AMBER':
+        os.system('%s %s' % (AMBER_PATH, fn))
+        fn_trigger = '%s.trigger' % fn_base
+    elif backend is 'PRESTO':
+        os.system('prepdata -start 0 -dm %d -o %s -ncpus 10 %s' % (DM, fn_base, fn_fil))        
+        os.system('single_pulse_search.py %s.dat -t %d -b' % (fn_base, SNR_MIN))
+        fn_trigger = '%s.singlepulse' % fn_base
+
+    os.system('python triggers.py %s %s --ntrig 500 --ndm 1 --save_data 0 --ntime_plot 750 --sig_thresh 15.' % (fn_fil, fn_trigger))
 exit()
 try:
     outfile_250 = glob.glob('%s/%s*fil' % (outdir, fn250))[-1]
