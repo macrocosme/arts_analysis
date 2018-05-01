fb3d4c749c1dc424134221e6587d9adc3f424aac
Liam Connor <connor@wcw-staff-145-109-83-36.wireless.uva.nl>
2018-05-01 15:26:21 +0200

    removing plotting stuff from triggers.py

--- triggers.py
+++ triggers.py
@@ -287,7 +287,9 @@ def proc_trigger(fn_fil, dm0, t0, sig_cut,
                     (dm_, start_bin*dt, downsamp))
         data_copy = copy.deepcopy(data)
         data_copy.dedisperse(dm_)
+        print(data_copy.data.shape)
         dm_arr = data_copy.data[:, t_min:t_max].mean(0)
+        print(dm_arr.shape)
 
         # Taken from PRESTO's single_pulse_search:
         # The following gets rid of (hopefully) most of the                                                                                                                
