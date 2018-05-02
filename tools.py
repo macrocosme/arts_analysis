582d94400e100cd3fab34b12447211ed14e4f24a
Liam Connor <connor@wcw-staff-145-109-85-89.wireless.uva.nl>
2018-05-02 17:03:01 +0200

    Rewrote the comparison trigger code. Now does things using arrays rather than lists. More sensible all round

--- tools.py
+++ tools.py
@@ -284,6 +284,7 @@ def compare_snr(fn_1, fn_2, dm_min=0, dm_max=np.inf, save_data=False,
 
     # concatenate list and reshape to (nparam, nmatch, 2 files)
     par_match_arr = np.concatenate(par_match_arr).reshape(-1, 4, 2)
+    print(par_match_arr.shape)
     par_match_arr = par_match_arr.transpose((1, 0, 2))
 
     if save_data is True:
