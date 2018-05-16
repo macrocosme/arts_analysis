fe6ee64ea2d5bc9ed313cbb0e97a8cdc38c0393e
Liam Connor <connor@wcw-staff-145-109-84-183.wireless.uva.nl>
2018-05-16 14:36:12 +0200

    Made grouped_pulses.singlepulse more human readable

--- tools.py
+++ tools.py
@@ -4,6 +4,13 @@ import scipy.signal
 # should there maybe be a clustering class
 # and a S/N calculation class?
 
+class AnalyseTriggers:
+
+    def __init__(self):
+        pass 
+
+
+
 def dm_range(dm_max, dm_min=5., frac=0.2):
     """ Generate list of DM-windows in which 
     to search for single pulse groups. 
@@ -144,6 +151,175 @@ def get_triggers(fn, sig_thresh=5.0, dm_min=0, dm_max=np.inf, t_window=0.5):
     print("Grouped down to %d triggers from %d\n" % (ntrig_group, ntrig_orig))
 
     return sig_cut, dm_cut, tt_cut, ds_cut
+
+class SNR_Tools:
+
+    def __init__(self):
+        pass
+
+    def sigma_from_mad(self, data):
+        """ Get gaussian std from median 
+        aboslute deviation (MAD)
+        """
+        assert len(data.shape)==1, 'data should be one dimensional'
+
+        med = np.median(data)
+        mad = np.median(np.absolute(data - med))
+
+        return 1.4826*mad, med
+
+    def calc_snr(self, data):
+        """ Calculate S/N of 1D input array (data)
+        after excluding 0.05 at tails
+        """
+        std_chunk = scipy.signal.detrend(data, type='linear')
+        std_chunk.sort()
+        ntime_r = len(std_chunk)
+        stds = 1.148*np.sqrt((std_chunk[ntime_r//40:-ntime_r//40]**2.0).sum() /
+                              (0.95*ntime_r))
+        snr_ = std_chunk[-1] / stds 
+
+        return snr_
+
+    def calc_snr_widths(self, data, widths=None):
+        """ Calculate the S/N of pulse profile after 
+        trying 9 rebinnings.
+
+        Parameters
+        ----------
+        arr   : np.array
+            (ntime,) vector of pulse profile 
+
+        Returns
+        -------
+        snr : np.float 
+            S/N of pulse
+        """
+        assert len(data.shape)==1
+        
+        ntime = len(data)
+        snr_max = 0
+        data -= np.median(data)
+
+        if widths is None:
+            widths = [1, 2, 4, 8, 16, 32, 64, 128]
+
+    #    for ii in range(1, 10):
+        for ii in widths:
+            for jj in range(ii):
+                # skip if boxcar width is greater than 1/4th ntime
+                if ii > ntime//8:
+                    continue
+                
+                arr_copy = data.copy()
+                arr_copy = np.roll(arr_copy, jj)
+                arr_ = arr_copy[:ntime//ii*ii].reshape(-1, ii).mean(-1)
+
+                snr_ = calc_snr(arr_)
+
+                if snr_ > snr_max:
+                    snr_max = snr_
+                    width_max = ii
+
+        return snr_max, width_max
+
+    def compare_snr(self, fn_1, fn_2, dm_min=0, dm_max=np.inf, save_data=False,
+                    sig_thresh=5.0, t_window=0.5):
+        """ Read in two files with single-pulse candidates
+        and compare triggers.
+
+        Parameters:
+        ----------
+        fn_1 : str 
+            name of input triggers text file
+            (must be .trigger, .singlepulse, or .txt)
+        fn_2 : str
+            name of input triggers text file for comparison 
+        dm_min : float
+            do not process triggers below this DM 
+        dm_max : float 
+            do not process triggers above this DM 
+        save_data : bool 
+            if True save to np.array
+        sig_thresh : float 
+            do not process triggers below this S/N 
+        t_window : float 
+            time window within which triggers in 
+            fn_1 and fn_2 will be considered the same 
+
+        Return:
+        -------
+        Function returns four parameter arrays for 
+        each fn_1 and fn_2, which should be ordered so 
+        that they can be compared directly:
+
+        grouped_params1, grouped_params2, matched_params
+        """
+        snr_1, dm_1, t_1, w_1 = get_triggers(fn_1, sig_thresh=sig_thresh, 
+                                    dm_min=dm_min, dm_max=np.inf, t_window=t_window)
+
+        snr_2, dm_2, t_2, w_2 = get_triggers(fn_2, sig_thresh=sig_thresh, 
+                                    dm_min=dm_min, dm_max=dm_max, t_window=t_window)
+
+        snr_2_reorder = []
+        dm_2_reorder = []
+        t_2_reorder = []
+        w_2_reorder = []
+
+        ntrig_1 = len(snr_1)
+        ntrig_2 = len(snr_2)    
+
+        par_1 = np.concatenate([snr_1, dm_1, t_1, w_1]).reshape(4, -1)
+        par_2 = np.concatenate([snr_2, dm_2, t_2, w_2]).reshape(4, -1)
+
+        # Make arrays for the matching parameters
+        par_match_arr = []
+        ind_missed = []
+
+        print("t_diff   t_0   t_1   dm_0   dm_1  snr_1   snr_2")
+        for ii in range(len(snr_1)):
+            tdiff = np.abs(t_1[ii] - t_2)
+            ind = np.where(tdiff == tdiff.min())[0]
+
+            # make sure you are getting correct trigger in dm/time space
+            if len(ind) > 1:
+                ind = ind[np.argmin(np.abs(dm_1[ii]-dm_2[ind]))]
+            else:
+                ind = ind[0]
+
+            # check for triggers that are within 1.0 seconds and 20% in dm
+            if (tdiff[ind]<1.0) and (np.abs(dm_1[ii]-dm_2[ind])/dm_1[ii])<0.2:
+                pparams = (tdiff[ind], t_1[ii], t_2[ind], dm_1[ii], dm_2[ind], snr_1[ii], snr_2[ind])
+                print("%1.4f  %5.1f  %5.1f  %5.1f  %5.1f %5.1f  %5.1f" % pparams)
+
+                params_match = np.array([snr_1[ii], snr_2[ind], 
+                                         dm_1[ii], dm_2[ind],
+                                         t_1[ii], t_2[ind],
+                                         w_1[ii], w_2[ind]])
+
+                par_match_arr.append(params_match)
+            else:
+                # Keep track of missed triggers
+                ind_missed.append(ii)
+
+        if len(par_match_arr)==0:
+            print("No matches found")
+            return 
+
+        # concatenate list and reshape to (nparam, nmatch, 2 files)
+        par_match_arr = np.concatenate(par_match_arr).reshape(-1, 4, 2)
+        par_match_arr = par_match_arr.transpose((1, 0, 2))
+
+        if save_data is True:
+            nsnr = min(len(snr_1), len(snr_2))
+            snr_1 = snr_1[:nsnr]
+            snr_2 = snr_2_reorder[:nsnr]
+
+            np.save(fn_1+'_params_grouped', par_1)
+            np.save(fn_2+'_params_grouped', par_2)
+            np.save('params_matched', par_match_1)
+
+        return par_1, par_2, par_match_arr, ind_missed    
 
 def sigma_from_mad(data):
     """ Get gaussian std from median 
@@ -309,6 +485,8 @@ def compare_snr(fn_1, fn_2, dm_min=0, dm_max=np.inf, save_data=False,
 
     return par_1, par_2, par_match_arr, ind_missed
 
+
+
 if __name__=='__main__':
 
     import sys
@@ -325,8 +503,10 @@ if __name__=='__main__':
     except:
         dm_max = np.inf
 
+    SNRTools = SNR_Tools()
+
     try:
-        par_1, par_2, par_match_arr = compare_snr(fn_1, fn_2, dm_min=dm_min, 
+        par_1, par_2, par_match_arr, ind_missed = SNRTools.compare_snr(fn_1, fn_2, dm_min=dm_min, 
                                         dm_max=dm_max, save_data=False,
                                         sig_thresh=5.0, t_window=0.13)
     except TypeError:
