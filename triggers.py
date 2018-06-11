195c154fbd672d5de551bd87ff8eaa645f726acb
ARTS <arts@arts041.apertif>
2018-06-11 13:00:10 +0000

    WRong call to get_triggers from tools.py

--- triggers.py
+++ triggers.py
@@ -469,7 +469,7 @@ if __name__=='__main__':
 
     SNRTools = tools.SNR_tools()
 
-    sig_cut, dm_cut, tt_cut, ds_cut = SNRTools.get_triggers(fn_sp, 
+    sig_cut, dm_cut, tt_cut, ds_cut = SNR_tools.get_triggers(fn_sp, 
                                                          sig_thresh=options.sig_thresh,
                                                          dm_min=options.dm_min,
                                                          dm_max=options.dm_max)
