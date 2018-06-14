de8472e3b6707df40724c3f92a8d644fab0becf2
ARTS <arts@arts041.apertif>
2018-06-14 13:56:54 +0000

    Plotted now takes as arguments frequency bounds

--- plotter.py
+++ plotter.py
@@ -90,6 +90,7 @@ def plot_three_panel(data_freq_time, data_dm_time, times, dms,
 def plot_from_h5(fn, cb, freq_low=1250.09765625, freq_up=1549.90234375, 
                  ):
     # read dataset 
+    print(freq_low, freq_up)
     with h5py.File(fn, 'r') as f:
         data_frb_candidate = f['data_frb_candidate'][:]
         probability = f['probability'][:]
@@ -109,50 +110,15 @@ if __name__ == '__main__':
     cb = int(sys.argv[2])
 
     try:
-        freq_low = sys.argv[3]
+        freq_low = np.float(sys.argv[3])
     except:
-        freq_low=1250.09765625
+        freq_low = 1250.09765625
 
     try:
-        freq_up = sys.argv[4]
+        freq_up = np.float(sys.argv[4])
     except:
         freq_up = 1549.90234375
         
     plot_from_h5(fn, cb, freq_low=freq_low, freq_up=freq_up)
-
-#     # read dataset 
-#     with h5py.File(fname, 'r') as f:
-#         data_frb_candidate = f['data_frb_candidate'][:]
-#         probability = f['probability'][:]
-#         params = f['params'][:]  # snr, DM, boxcar width, arrival time
-
-#     for i, cand in enumerate(data_frb_candidate):
-#         data_freq_time = cand[:, :, 0]
-#         prob = probability[i]
-#         snr, dm, bin_width, t0 = params[i]
-
-#         times = np.arange(data_freq_time.shape[1]) * bin_width * 1E3  # ms
-#         fmin = 1250.09765625
-#         fmax = 1549.90234375
-#         freqs = np.linspace(fmin, fmax, data_freq_time.shape[0])
-
-#         fig, (ax1, ax2) = plt.subplots(nrows=2, sharex=True, gridspec_kw=dict(height_ratios=[1, 2]))
-
-#         # timeseries
-#         ax1.plot(times, np.sum(data_freq_time, axis=0)/np.sqrt(data_freq_time.shape[0]), c='k')
-#         ax1.set_ylabel('S/N')
-#         # add what a DM=0 signal would look like
-#         DM0_delays = dm * 4.15E6 * (fmin**-2 - freqs**-2)
-#         ax2.plot(DM0_delays, freqs, c='r', lw='2')
-#         # waterfall plot
-#         # scaling: std = 1, median=0
-#         extent = [times[0], times[-1], fmin, fmax]
-#         ax2.imshow(data_freq_time, cmap='viridis', vmin=-3, vmax=3, interpolation='nearest', aspect='auto', origin='upper', extent=extent)
-#         ax2.set_xlabel('Time (ms)')
-#         ax2.set_ylabel('Freq (MHz)')
-#         fig.suptitle("p: {:.2f}, S/N: {:.0f}, DM: {:.2f}, T0: {:.2f}, CB: {:02d}".format(prob, snr, dm, t0, cb))
-#         plt.savefig("plots/cand_{:04d}_snr{:.0f}_dm{:.0f}.pdf".format(i, snr, dm))
-#         plt.close(fig)
-
 
 
